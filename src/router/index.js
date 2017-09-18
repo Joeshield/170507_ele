7fc28ec9e8d40fea8565e9717c82e6d0d6ffd647
zxfjd3g <258147149@qq.com>
2017-09-18 16:25:34 +0800

    路由组件懒加载

--- src/router/index.js
+++ src/router/index.js
@@ -3,9 +3,16 @@
  */
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+/*
+
 import goods from '../components/goods/goods.vue'
 import ratings from '../components/ratings/ratings.vue'
 import seller from '../components/seller/seller.vue'
+*/
+// 路由组件懒加载: 将路由组件单独打包, 只有第一次请求此路由地址时才会加载此路由组件包
+const goods = () => import('../components/goods/goods.vue')
+const ratings = () => import('../components/ratings/ratings.vue')
+const seller = () => import('../components/seller/seller.vue')
 
 Vue.use(VueRouter)
 
